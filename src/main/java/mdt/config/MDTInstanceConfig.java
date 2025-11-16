4e38772f04e1c529cddde5b4392df14ad7418322
Kang-Woo Lee <kwlee@etri.re.kr>
2025-11-16 16:31:38 +0900

    add comments to MDTInstanceConfig for better code clarity

--- src/main/java/mdt/config/MDTInstanceConfig.java
+++ src/main/java/mdt/config/MDTInstanceConfig.java
@@ -23,23 +23,30 @@ import mdt.persistence.timeseries.TimeSeriesSubmodelConfig;
 @Getter @Setter
 @Accessors(prefix="m_")
 public class MDTInstanceConfig {
-	private String m_id;
-	private Integer m_port;
-	private String m_instanceEndpoint;
-	private String m_managerEndpoint;
+	private String m_id;					// MDT 인스턴스 아이디
+	private Integer m_port;					// MDT 인스턴스 포트
+	private String m_instanceEndpoint;		// MDT 인스턴스에 부여된 endpoint
+	private String m_managerEndpoint;		// MDTManager endpoint
 	
-	private File m_globalConfigFile;
-	private File m_keyStoreFile;
-	private String m_keyStorePassword;
-	private String m_keyPassword;
+	private File m_globalConfigFile;		// 글로벌 설정 파일 경로명
+	private File m_keyStoreFile;			// Key Store 파일 경로명
+	private String m_keyStorePassword;		// Key Store 암호
+	private String m_keyPassword;			// Key 암호
 	
-	private String m_heartbeatInterval;
-	private String m_managerCheckInterval;
+	private String m_heartbeatInterval;		// (External MDTInstance인 경우) MDTInstance에서 MDTManager로
+											// 재접속을 시도하는 주기
+	private String m_managerCheckInterval;	// (External MDTInstance가 아닌 경우) MDTInstance에서 주기적으로
+											// MDTManager 상태 점검 주기
 	
+	// 본 MDTInstance에서 제공하는 서비스 엔드포인트 설정. mqtt, ros2, companion 등
 	private @Nullable ServiceEndpointConfigs m_serviceEndpoints;
+	// 본 MDTInstance에 등록된 자산 변수 설정 목록. 예: Jdbc, mqtt, opcua 등
 	private List<AssetVariableConfig> m_assetVariables = Lists.newArrayList();
+	// 본 MDTInstance에 설정된 영속성 스택 목록
 	private List<MDTPersistenceStackConfig> m_persistenceStacks = Lists.newArrayList();
+	// 본 MDTInstance에 설정된 시계열 서브모델 목록
 	private List<TimeSeriesSubmodelConfig> m_timeSeriesSubmodels = Lists.newArrayList();
+	// 본 MDTInstance에서 제공하는 AAS 연산 설정 목록
 	private OperationsConfig m_operations;
 	
 	public String getSubmodelEndpoint(String submodelId) {
